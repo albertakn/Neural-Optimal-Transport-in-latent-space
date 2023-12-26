a7a5480d884fe8faa85231c6d8dff39919d3e3ae
albertakn <akopian.as@phystech.edu>
2023-12-26 15:41:31 +0000

    add utils.py

--- src/tools/utils.py
+++ src/tools/utils.py
@@ -6,7 +6,20 @@ def freeze(model):
         p.requires_grad_(False)
     model.eval()
 
+
 def unfreeze(model):
     for p in model.parameters():
         p.requires_grad_(True)
     model.train(True)
+
+
+def rescale(x, old_range, new_range, clamp=False):
+    old_min, old_max = old_range
+    new_min, new_max = new_range
+    x -= old_min
+    x *= (new_max - new_min) / (old_max - old_min)
+    x += new_min
+    if clamp:
+        x = x.clamp(new_min, new_max)
+    return x
+
